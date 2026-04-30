14c2fb81faeb3848e9184fcbb2c15bd2c0606483
A02368805 <a02368805@usu.edu>
2026-04-30 11:43:40 -0600

    raise max_candidates and resultlimit

--- lib/msp/constants.ts
+++ lib/msp/constants.ts
@@ -1,10 +1,10 @@
 export const MAX_SEARCH_QUERIES = 8;
 export const MAX_SOURCES_FETCHED = 24;
-export const MAX_CANDIDATES = 20;
-export const MAX_FINAL_RESULTS = 20;
+export const MAX_CANDIDATES = 50;
+export const MAX_FINAL_RESULTS = 50;
 export const RESEARCH_ROUNDS = 3;
 
-export const DEFAULT_RESULT_LIMIT = 20;
+export const DEFAULT_RESULT_LIMIT = 50;
 
 export const SUPPORTED_SOURCE_TYPES = [
   "official_website",
